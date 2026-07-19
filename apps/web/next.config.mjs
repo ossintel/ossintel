e018fe4fadb97e82118320289e4108e66e828930
Mayank <mayank.srmu@gmail.com>
2026-07-19 22:50:32 +0530

    fix: :bug: remove github specific static export - we require dynamic routes for this project

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,5 @@
 import { createMDX } from "fumadocs-mdx/next";
 
-/** Detect GitHub Actions */
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
@@ -12,10 +9,6 @@ const config = {
   images: {
     unoptimized: true,
   },
-  ...(isGitHubActions && {
-    // basePath: "/turboforge", // we are publishing to https://turboforge.js.org/ -- so remove basePath
-    output: "export",
-  }),
 };
 
 export default withMDX(config);
